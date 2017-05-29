e992cfee93604f601974549e35625b8d2a8d9fcb
Kelsey Huse <kelseyhuse30@gmail.com>
2017-05-29 15:31:11 -0400

    fixed alcoholic helper method

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,6 +1,6 @@
 module CocktailsHelper
 
 	def alcoholic?(cocktail)
-		cocktail.alcolic ? "Alcoholic Beverage" : "Non-alcoholic"
+		cocktail.alcoholic ? "Alcoholic Beverage" : "Non-alcoholic"
 	end
 end
