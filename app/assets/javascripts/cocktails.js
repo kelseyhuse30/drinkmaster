15db33be456866f90d8f561ebed9e5fac9cc7f0f
Kelsey Huse <kelseyhuse30@gmail.com>
2017-06-16 18:47:01 -0400

    starting ajax to load comments

--- app/assets/javascripts/cocktails.js
+++ app/assets/javascripts/cocktails.js
@@ -1,10 +1,29 @@
-$(function() {
+$(function(){
 	attachListeners();
 });
 
 function attachListeners(){
 	$("#comments").on("click", function(){
-		debugger;
-		getComments()
+		var id = $(this).data("id");
+		getComments(id)
 	})
-}
+};
+
+function getComments(id) {
+	$.ajax({
+		method: 'get',
+		url: '/cocktails/' + id + '/comments'
+	})
+	.done(function(resp) {
+		debugger;
+	})
+};
+
+function populateComments(comments){
+if (comments.length > 0){
+	var list = $("#commentDiv").append('<ul></ul>').find('ul');
+			comments.forEach(function(comment){
+				list.append('<li id="' + comment["id"] + '">' + comment["comment"] + "</li>")
+			})
+		}
+};
