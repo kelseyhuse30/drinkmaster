b754b6f2f274a4549cbc9e2170f3743f2421a531
Kelsey Huse <kelseyhuse30@gmail.com>
2017-06-17 10:44:30 -0400

    rendering next cocktail in show with ajax successfully

--- app/assets/javascripts/cocktails.js
+++ app/assets/javascripts/cocktails.js
@@ -40,6 +40,32 @@ function getCocktail(id) {
 		dataType: 'json'
 	})
 	.done(function(resp) {
-		debugger;
+		populateFields(resp["cocktail"]);
 	})
+}
+
+function populateFields(cocktail){
+	$("#name").text(cocktail["name"]);
+	renderAlcoholic(cocktail["alcoholic"])
+	$("#email").text(cocktail["user"]["email"]);
+	$("#instructions").text(cocktail["instructions"]);
+	populateIngredients(cocktail["cocktail_ingredients"]);
+	$("#edit").attr("href", `/cocktails/${cocktail["id"]}/edit`);
+	$("#next").data("id", `${cocktail["id"] + 1}`);
+	$(".comments").data("id", `${cocktail["id"]}`);
+}
+
+function renderAlcoholic(bool) {
+	var result = bool ? "Alcoholic Beverage" : "Non-alcoholic"
+	$("#alcoholic").text(result);
+}
+
+function populateIngredients(ingredients) {
+	var list = $("#ingredientsList");
+	list.empty();
+	if (ingredients.length > 0){
+		ingredients.forEach(function(cocktail_ingredient){
+			list.append(`<li> ${cocktail_ingredient["quantity"]} of ${cocktail_ingredient["ingredient"]["name"]} </li>`)
+			});
+		}
 }
